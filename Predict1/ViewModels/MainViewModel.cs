e829e2834047523f8253069edc17422c297065d5
Gloreus <slavva01@yandex.ru>
2025-03-05 23:43:39 +0300

    + TemplateSelector

--- Predict1/ViewModels/MainViewModel.cs
+++ Predict1/ViewModels/MainViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Data.Converters;
 using Avalonia.Media;
+using Avalonia.Metadata;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 
@@ -18,13 +23,39 @@ public class Gem
 {
     public int Status { get; set; } = 0;
     public GemKind Kind { get; set; } = 0;
+    public override string ToString()
+    {
+        return $"Gem_{Kind}";
+    }
 }
+
+public class MyTemplateSelector : IDataTemplate
+{
+    public bool SupportsRecycling => false;
+    [Content]
+    public Dictionary<string, IDataTemplate> Templates {get;} = new Dictionary<string, IDataTemplate>();
+
+    
+    public bool Match(object? data)
+    {
+        return data is Gem;
+    }
+
+    public Control? Build(object? data)
+    {
+        if (data is Gem gem)
+            return Templates[gem.ToString()].Build(data);
+        else return null;
+    }
+}
+
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
-    public const int ColCount = 14;
-    public const int RowCount = 14;
+
+    public static int ColCount { get; set; } = 14;
+    public static int RowCount { get; set; } = 14;
 
     static readonly IImmutableSolidColorBrush[] gemColors =
     [
@@ -43,7 +74,7 @@ public partial class MainViewModel : ViewModelBase
         for (int i = 0; i < RowCount; i++)
         for (int j = 0; j < ColCount; j++)
         {
-            int c = rnd.Next(0, gemColors.Length);
+            int c = rnd.Next(0, Enum.GetNames(typeof(GemKind)).Length);
             _gems.Add(new Gem {Status = 0, Kind = (GemKind) c });
         }
     }
