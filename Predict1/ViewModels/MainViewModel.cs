5b00c27896a011337a67129c0256688da4804a8c
Gloreus <slavva01@yandex.ru>
2025-03-09 21:27:01 +0300

    + Возможность задать стиль отоборажения камня

--- Predict1/ViewModels/MainViewModel.cs
+++ Predict1/ViewModels/MainViewModel.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using Avalonia.Controls;
-using Avalonia.Controls.Metadata;
+using Avalonia;
 using Avalonia.Controls.Shapes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MagicGems.Core;
 
 namespace Predict1.ViewModels;
 
-[PseudoClasses(":Hot", ":Light")]
-public class Gem : Rectangle
-{
-    public int Color { get; }
-    private bool _hot = false;
 
+public class GemControl:Rectangle
+{
+    public int GemColor { get; set; }
+    private bool _hot = false;
+    public static readonly StyledProperty<int> GemColorProperty =
+        AvaloniaProperty.Register<GemControl, int>(nameof(GemColor), defaultValue: 0);
     public bool Hot
-    {
+    {   
         get => _hot;
         set
         {
             _hot = value;
-            PseudoClasses.Set(":Hot", value);
         }
     }
 
@@ -32,20 +32,36 @@ public class Gem : Rectangle
         set
         {
             _ligth = value;
-            PseudoClasses.Set(":Ligth", value);
         }
     }
+
+    public GemControl(int color, bool hot)
+    {
+        GemColor = color;
+        Hot = hot;
+        Ligth = false;
+    }
+
+    public GemControl() : this(0, false)
+    {
+    }
+    
+    public override string ToString()
+    {
+        return GemColor.ToString();
+    }
+}
+
+public class Gem
+{
+    public int Color { get; set; } = 0;
+    public bool Hot { get; set; } = false;
 
     public Gem(int color, bool hot)
     {
         Color = color;
         Hot = hot;
     }
-
-    public Gem() : this(0, false)
-    {
-    }
-
 }
 
 public partial class MainViewModel : ViewModelBase
@@ -62,16 +78,29 @@ public partial class MainViewModel : ViewModelBase
     public int RowCount { get => _desk.RowCount; }
     public int ColumnCount { get => _desk.ColumnCount; }
 
-    private ObservableCollection<Gem> GemList { get; } = new ObservableCollection<Gem>();
+    [ObservableProperty]
+    private ObservableCollection<Gem> _gemList  = new ObservableCollection<Gem>();
 
     public MainViewModel()
     {
         _desk = new GemDesk(5, 5, _gemColors.Count);
+        RefreshGems();
+    }
+
+    private void RefreshGems()
+    {
         GemList.Clear();
         foreach (int i in _desk.Items)
         {
             Gem g = new Gem(i, false);
             GemList.Add(g);
         }
+    }
+
+    [RelayCommand]
+    private void Start()
+    {
+        _ = _desk.Shuffle();
+        RefreshGems();
     }
 }
